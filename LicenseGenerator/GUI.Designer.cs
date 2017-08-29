8308090e43e37f1e43b050e0eb13821c0b53bdd7
Ben Lenington <ben2lening@gmail.com>
2017-08-28 21:35:08 -0500

    Initial commit: Move source files into new repository

---
+++ LicenseGenerator/GUI.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace LicenseGenerator
+{
+    partial class Gui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextboxUsername = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TextboxLicenseKey = new System.Windows.Forms.TextBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextboxSecretKey = new System.Windows.Forms.TextBox();
+            this.ProductComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EncryptionComboBox = new System.Windows.Forms.ComboBox();
+            this.ValidateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextboxUsername
+            // 
+            this.TextboxUsername.Location = new System.Drawing.Point(175, 239);
+            this.TextboxUsername.Name = "TextboxUsername";
+            this.TextboxUsername.Size = new System.Drawing.Size(392, 31);
+            this.TextboxUsername.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // TextboxLicenseKey
+            // 
+            this.TextboxLicenseKey.Location = new System.Drawing.Point(175, 292);
+            this.TextboxLicenseKey.Name = "TextboxLicenseKey";
+            this.TextboxLicenseKey.Size = new System.Drawing.Size(392, 31);
+            this.TextboxLicenseKey.TabIndex = 2;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(242, 358);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(160, 54);
+            this.GenerateButton.TabIndex = 4;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButtonClick);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(27, 358);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(160, 54);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButtonClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "License Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Secret Key";
+            // 
+            // TextboxSecretKey
+            // 
+            this.TextboxSecretKey.Location = new System.Drawing.Point(175, 189);
+            this.TextboxSecretKey.Name = "TextboxSecretKey";
+            this.TextboxSecretKey.Size = new System.Drawing.Size(392, 31);
+            this.TextboxSecretKey.TabIndex = 8;
+            // 
+            // ProductComboBox
+            // 
+            this.ProductComboBox.FormattingEnabled = true;
+            this.ProductComboBox.Location = new System.Drawing.Point(114, 90);
+            this.ProductComboBox.Name = "ProductComboBox";
+            this.ProductComboBox.Size = new System.Drawing.Size(266, 33);
+            this.ProductComboBox.TabIndex = 10;
+            this.ProductComboBox.SelectedIndexChanged += new System.EventHandler(this.ProductMenuSelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Product";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(417, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Encryption";
+            // 
+            // EncryptionComboBox
+            // 
+            this.EncryptionComboBox.FormattingEnabled = true;
+            this.EncryptionComboBox.Location = new System.Drawing.Point(537, 90);
+            this.EncryptionComboBox.Name = "EncryptionComboBox";
+            this.EncryptionComboBox.Size = new System.Drawing.Size(121, 33);
+            this.EncryptionComboBox.TabIndex = 12;
+            // 
+            // ValidateButton
+            // 
+            this.ValidateButton.Location = new System.Drawing.Point(447, 358);
+            this.ValidateButton.Name = "ValidateButton";
+            this.ValidateButton.Size = new System.Drawing.Size(160, 54);
+            this.ValidateButton.TabIndex = 16;
+            this.ValidateButton.Text = "Vaildate";
+            this.ValidateButton.UseVisualStyleBackColor = true;
+            this.ValidateButton.Click += new System.EventHandler(this.ValidateButtonClick);
+            // 
+            // Gui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 515);
+            this.Controls.Add(this.ValidateButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.EncryptionComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ProductComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextboxSecretKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.TextboxLicenseKey);
+            this.Controls.Add(this.TextboxUsername);
+            this.Name = "Gui";
+            this.Text = "License Generator (Lenington Design)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextboxUsername;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox TextboxLicenseKey;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextboxSecretKey;
+        private System.Windows.Forms.ComboBox ProductComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox EncryptionComboBox;
+        private System.Windows.Forms.Button ValidateButton;
+    }
+}
+
---
+++ LicenseGenerator/GUI.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LicenseGenerator
+{
+    public partial class Gui : Form
+    {
+        #region Properties
+
+        /// <summary>
+        /// The private hash key for the UniCade interface
+        /// </summary>
+        private const string UniCadeKey = "JI3vgsD6Nc6VSMrNw0b4wvuJmDw6Lrld";
+
+        /// <summary>
+        /// The private hash key for the gust list software
+        /// </summary>
+        private const string GuestListKey = "bjx58tQvp2Y1U5xO71PIsvFL7FGyyI08";
+
+        #endregion
+
+        #region  Public Methods
+
+        /// <summary>
+        /// Public constructor for the GUI
+        /// </summary>
+        public Gui()
+        {
+            //Initialize the GUI window
+            InitializeComponent();
+
+            //Populate combo boxes
+            ProductComboBox.Items.Add("UniCade Interface");
+            ProductComboBox.Items.Add("Guest List Software");
+            EncryptionComboBox.Items.Add("SHA1");
+            EncryptionComboBox.Items.Add("SHA256");
+
+            //Change the selected indexes
+            ProductComboBox.SelectedIndex = 0;
+            EncryptionComboBox.SelectedIndex = 1;
+        }
+
+        #endregion
+
+        #region  Private Methods
+
+        /// <summary>
+        /// Generate a new license key
+        /// </summary>
+        private void GenerateButtonClick(object sender, EventArgs e)
+        {
+            if ((TextboxUsername.Text.Length < 1) || (TextboxSecretKey.Text.Length < 1)  || (EncryptionComboBox.SelectedItem==null))
+            {
+                MessageBox.Show(Strings.RequiredFieldsEmpty);
+                return;
+            }
+            if (EncryptionComboBox.SelectedItem.ToString().Equals("SHA1"))
+            {
+                TextboxLicenseKey.Text = Sha1Hash(TextboxUsername.Text + TextboxSecretKey.Text);
+            }
+            else if (EncryptionComboBox.SelectedItem.ToString().Equals("SHA256"))
+            {
+                TextboxLicenseKey.Text = Sha256Hash(TextboxUsername.Text + TextboxSecretKey.Text);
+            }
+        }
+
+        /// <summary>
+        /// Exit the program
+        /// </summary>
+        private void CloseButtonClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ValidateButtonClick(object sender, EventArgs e)
+        {
+            if ((TextboxUsername.Text.Length < 1) || (TextboxSecretKey.Text.Length < 1) || (TextboxLicenseKey.Text.Length < 1) || (EncryptionComboBox.SelectedItem == null))
+            {
+                MessageBox.Show(Strings.RequiredFieldsEmpty);
+                return;
+            }
+            if (EncryptionComboBox.SelectedItem.ToString().Equals("SHA1"))
+            {
+                MessageBox.Show(ValidateSha1((TextboxUsername.Text + TextboxSecretKey.Text), TextboxLicenseKey.Text)
+                    ? Strings.KeyValid
+                    : Strings.KeyInvalid);
+            }
+            else if (EncryptionComboBox.SelectedItem.ToString().Equals("SHA256"))
+            {
+                MessageBox.Show(ValidateSha256((TextboxUsername.Text + TextboxSecretKey.Text), TextboxLicenseKey.Text)
+                    ? Strings.KeyValid
+                    : Strings.KeyInvalid);
+            }
+        }
+
+        /// <summary>
+        /// Method called when the selected product is changed
+        /// </summary>
+        private void ProductMenuSelectedIndexChanged(object sender, EventArgs e) 
+        {
+            switch (ProductComboBox.SelectedIndex)
+            {
+                case 0:
+                    TextboxSecretKey.Text = UniCadeKey;
+                    break;
+                case 1:
+                    TextboxSecretKey.Text = GuestListKey;
+                    break;
+                default:
+                    TextboxSecretKey.Text = null;
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Generate a new string with the SHA1 hash algorithim
+        /// </summary>
+        private static string Sha1Hash(string data)
+        {
+            SHA1 sha1 = SHA1.Create();
+            byte[] hashData = sha1.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder returnValue = new StringBuilder();
+
+            foreach (byte t in hashData)
+            {
+                returnValue.Append(t.ToString());
+            }
+            return returnValue.ToString();
+        }
+
+        /// <summary>
+        /// Generate a new string with the SHA256 hash algorithim
+        /// </summary>
+        private static string Sha256Hash(string data)
+        {
+            SHA256Managed sha256 = new SHA256Managed();
+            byte[] hashData = sha256.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder returnValue = new StringBuilder();
+
+            foreach (byte t in hashData)
+            {
+                returnValue.Append(t.ToString());
+            }
+            return returnValue.ToString();
+        }
+
+        /// <summary>
+        /// Validate a string against a SHA1 hash key
+        /// </summary>
+        private static bool ValidateSha1(string input, string storedHashData)
+        {
+            string getHashInputData = Sha1Hash(input);
+
+            return string.CompareOrdinal(getHashInputData, storedHashData) == 0;
+        }
+
+        /// <summary>
+        /// Validate a string against a SHA257 hash key
+        /// </summary>
+        private static bool ValidateSha256(string input, string storedHashData)
+        {
+            string getHashInputData = Sha256Hash(input);
+
+            return string.CompareOrdinal(getHashInputData, storedHashData) == 0;
+        }
+
+        #endregion
+    }
+}
